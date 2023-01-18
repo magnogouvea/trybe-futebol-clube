c9c804da23e159a25d6f3c4fbdd92c3f49d461ba
magnogouvea <magnogouvea@gmail.com>
2023-01-18 10:05:30 -0300

    Feat: Adiciona req 26

--- app/backend/src/middleware/matchesValidation.ts
+++ app/backend/src/middleware/matchesValidation.ts
@@ -1,12 +1,25 @@
 import { NextFunction, Request, Response } from 'express';
+import TeamsService from '../services/TeamsService';
 import IError from '../entities/IError';
 
-const matchesValidation = (req: Request, _res: Response, next: NextFunction): void => {
+const matchesValidation = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
   const { homeTeam, awayTeam } = req.body;
   if (homeTeam === awayTeam) {
     const error: IError = new Error('It is not possible to create a match with two equal teams');
     error.status = 422;
     throw error;
+  }
+
+  const teamsService = new TeamsService();
+  const firstTeam = await teamsService.findTeamById(homeTeam);
+  const secondTeam = await teamsService.findTeamById(awayTeam);
+
+  if (!firstTeam || !secondTeam) {
+    return res.status(404).json({ message: 'There is no team with such id!' });
   }
   next();
 };
